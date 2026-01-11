cd6d7602cf28db3dddfc2e41d3100aadd96fa22f
Lynton <lazumlyntonkolay@gmail.com>
2026-01-11 08:29:13 +0630

    Update database configuration to use environment variables

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'host' => '127.0.0.1',
-    'db' => 'vending_machine',
-    'user' => 'root',
-    'pass' => 'root',
+    'host' => getenv('MYSQLHOST') ?: '127.0.0.1',
+    'db' => getenv('MYSQLDATABASE') ?: 'vending_machine',
+    'user' => getenv('MYSQLUSER') ?: 'root',
+    'pass' => getenv('MYSQLPASSWORD') ?: 'root',
     'charset' => 'utf8mb4'
 ];
